05375a9fc5154ce24f5e76cf3a5282147efb4214
Vidyasagar Machupalli <vidyasagar.msc@in.ibm.com>
2020-11-09 22:15:46 +0530

    Updated mc install

--- delete-cos-objects.sh
+++ delete-cos-objects.sh
@@ -4,5 +4,5 @@ wget https://dl.min.io/client/mc/release/linux-amd64/mc
 chmod +x mc
 
 echo "Configuring MINIO client...."
-mc config host add cos https://s3.$1.cloud-object-storage.appdomain.cloud $2 $3
-mc rm --recursive --force cos/$4
+./mc config host add cos https://s3.$1.cloud-object-storage.appdomain.cloud $2 $3
+./mc rm --recursive --force cos/$4
